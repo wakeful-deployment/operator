c9b0d6dc0309b39c0158783c169adabdb163aaf3
Ryan Levick <ryan.levick@microsoft.com>
2015-12-03 18:26:26 +0100

    Fetch new state

--- operator.go
+++ operator.go
@@ -7,61 +7,98 @@ import (
   "encoding/json"
   "syscall"
   "strconv"
+  "strings"
   "time"
+  "errors"
 )
+
+type KeyState struct {
+  Index int
+  Keys []ConsulKey
+}
 
 type ConsulKey struct {
   Key string
   ModifyIndex int
 }
 
-func handleConsulKeys(resps []ConsulKey, currentIndex int) {
-  for _, resp := range resps {
-    if (resp.ModifyIndex > currentIndex) {
-      fmt.Println("The key is more advanced than we currently know about")
+func handleConsulKeys(keys []ConsulKey, currentIndex int) {
+  for _, key := range keys {
+    if (strings.HasPrefix(key.Key, "apps")) {
+      fmt.Println("HEY")
     }
+  }
+}
+
+func url(hostname string, index int) string {
+  return fmt.Sprintf("http://192.168.99.100:8500/v1/kv/_wakeful/nodes/%s?recurse=true&index=%d", hostname, index)
+}
+
+func getNewKeyState(hostname string, state KeyState) (KeyState, error) {
+  resp, err := http.Get(url(hostname, state.Index))
+
+  if err != nil {
+    var emptyState KeyState
+    return emptyState, err
+  }
+
+  switch resp.StatusCode {
+  case 200:
+    index, err := strconv.Atoi(resp.Header["X-Consul-Index"][0])
+
+    if err != nil {
+      var emptyState KeyState
+      return emptyState, err
+    }
+
+    var keys []ConsulKey
+    err = json.NewDecoder(resp.Body).Decode(&keys)
+
+    if err !=  nil {
+      var emptyState KeyState
+      return emptyState, err
+    }
+
+    state.Keys = keys
+    state.Index = index
+
+    return state, nil
+  case 404:
+    index, err := strconv.Atoi(resp.Header["X-Consul-Index"][0])
+
+    if err != nil {
+      var emptyState KeyState
+      return emptyState, err
+    }
+    state.Index = index
+    state.Keys = []ConsulKey{}
+    return state, nil
+  default:
+    var emptyState KeyState
+    return emptyState, errors.New("non 200/404 response code")
   }
 }
 
 func main () {
   hostname, _ := os.LookupEnv("HOSTNAME")
-  var index = 0
+  state := KeyState{Keys: []ConsulKey{}, Index: 0}
 
   for {
-    var url = fmt.Sprintf("http://192.168.99.100:8500/v1/kv/_wakeful/nodes/%s?recurse=true&index=%d", hostname, index)
-    resp, err := http.Get(url)
+    newState, err := getNewKeyState(hostname, state)
+
+    // handleStateChange(state, newState)
+
+    for _, key := range newState.Keys {
+      fmt.Println(key)
+    }
 
     if err != nil {
-      fmt.Println("There was an error connecting to consul.")
+      fmt.Println("There was an error getting the new state")
       syscall.Exit(1)
     }
 
-    switch resp.StatusCode {
-    case 200:
-      newIndex, err := strconv.Atoi(resp.Header["X-Consul-Index"][0])
+    time.Sleep(time.Second)
 
-      if err != nil {
-        fmt.Println("There was no 'X-Consul-Index' header...")
-        syscall.Exit(1)
-      }
-
-      var consulKeys []ConsulKey
-      err = json.NewDecoder(resp.Body).Decode(&consulKeys)
-
-      if err != nil {
-        fmt.Println("There was an error decoding consule response json that we need to handle.")
-        syscall.Exit(1)
-      }
-
-      handleConsulKeys(consulKeys, index)
-
-      index = newIndex
-    case 404:
-      fmt.Println("No keys in keyspace were found. Trying again in one second...")
-      time.Sleep(time.Second)
-    default:
-      fmt.Println("Response was neither 200 nor 404. Something is wrong...")
-      time.Sleep(time.Second)
-    }
+    state = newState
   }
 }
