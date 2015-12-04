b1d970b58fb17ce16c482e3ff640f1c695184caa
nathan-ms <nathan.herald@microsoft.com>
2015-12-04 19:20:29 +0100

    Always compare the current containers to the desired state

--- operator.go
+++ operator.go
@@ -14,22 +14,52 @@ import (
 	"time"
 )
 
-type KeyState struct {
-	Index int
-	Keys  []ConsulKey
+type Container struct {
+	Name  string
+	Image string
 }
 
-type ConsulKey struct {
+type ConsulState struct {
+	Index int
+	KVs   []ConsulKV
+}
+
+type ConsulKV struct {
 	Key         string
 	Value       string
 	ModifyIndex int
+}
+
+func (c ConsulKV) ContainerName() string {
+	keyParts := strings.Split(c.Key, "/")
+	return keyParts[len(keyParts)-1]
+}
+
+func (c ConsulKV) ContainerImage() string {
+	base64Value := c.Value
+	decoded, _ := base64.StdEncoding.DecodeString(base64Value)
+	return string(decoded)
+}
+
+func (c ConsulKV) ToContainer() Container {
+	return Container{Name: c.ContainerName(), Image: c.ContainerImage()}
+}
+
+func (c ConsulState) Containers() []Container {
+	var containers []Container
+
+	for _, kv := range c.KVs {
+		containers = append(containers, kv.ToContainer())
+	}
+
+	return containers
 }
 
 func makeUrl(consulHost string, hostname string, index int) string {
 	return fmt.Sprintf("http://%s:8500/v1/kv/_wakeful/nodes/%s?recurse=true&index=%d", consulHost, hostname, index)
 }
 
-func getKeyState(state *KeyState, url string) error {
+func getConsulStateBlocking(state *ConsulState, url string) error {
 	resp, err := http.Get(url)
 
 	if err != nil {
@@ -44,14 +74,14 @@ func getKeyState(state *KeyState, url string) error {
 			return err
 		}
 
-		var keys []ConsulKey
+		var keys []ConsulKV
 		err = json.NewDecoder(resp.Body).Decode(&keys)
 
 		if err != nil {
 			return err
 		}
 
-		state.Keys = keys
+		state.KVs = keys
 		state.Index = index
 
 		return nil
@@ -63,7 +93,7 @@ func getKeyState(state *KeyState, url string) error {
 		}
 
 		state.Index = index
-		state.Keys = []ConsulKey{}
+		state.KVs = []ConsulKV{}
 
 		return nil
 	default:
@@ -71,87 +101,102 @@ func getKeyState(state *KeyState, url string) error {
 	}
 }
 
-// Find keys that are present in first slice that are not present in second
-func keyDiff(keySlice1 []ConsulKey, keySlice2 []ConsulKey) []ConsulKey {
-	var diff []ConsulKey
+func containerIsWhitelisted(container Container) bool {
+	containerWhiteList := []string{"consul", "statsite", "operator"}
+	for _, name := range containerWhiteList {
+		if container.Name == name {
+			return true
+		}
+	}
 
-	for _, keyInFirst := range keySlice1 {
-		var present = true
-		for _, keyInSecond := range keySlice2 {
-			if keyInFirst.Key == keyInSecond.Key {
-				present = false
-				break
-			}
+	return false
+}
+
+// Find keys that are present in first slice that are not present in second
+func diffContainers(leftContainers []Container, rightContainers []Container) []Container {
+	var diff []Container
+
+	for _, left := range leftContainers {
+		if containerIsWhitelisted(left) {
+			continue
 		}
 
-		if present {
-			diff = append(diff, keyInFirst)
+		for _, right := range rightContainers {
+			if left.Name == right.Name {
+				diff = append(diff, left)
+				break
+			}
 		}
 	}
 
 	return diff
 }
 
-func runningContainers() ([]string, error) {
+func runningContainers() ([]Container, error) {
 	psOut, err := exec.Command("docker", "ps").Output()
 
 	if err != nil {
-		return []string{}, err
+		return []Container{}, err
 	}
 
-	var runningContainers []string
+	var runningContainers []Container
+
 	lines := strings.Split(string(psOut), "\n")
+
 	for index, line := range lines {
 		if index == 0 {
 			continue
 		}
 
-		containerInfo := strings.Split(line, " ")
-		containerName := containerInfo[len(containerInfo)-1]
-		containerName = strings.Trim(containerName, " ")
+		info := strings.Split(line, " ")
 
-		if len(containerName) > 0 {
-			runningContainers = append(runningContainers, containerName)
+		var name string
+		var image string
+
+		if len(info) < 2 {
+			fmt.Printf("retreived info was not formatted correctly: %v\n", info)
+			continue
+		} else {
+			fmt.Printf("info: %v\n", info)
+
+			name = info[len(info)-1]
+			name = strings.Trim(name, " ")
+
+			image = info[1]
+			image = strings.Trim(image, " ")
+		}
+
+		if len(name) > 0 {
+			container := Container{Name: name, Image: image}
+			runningContainers = append(runningContainers, container)
 		}
 	}
 
 	return runningContainers, nil
 }
 
-func containerIsRunning(containerName string, runningContainers []string) bool {
-	containerIsRunning := false
-
-	for _, runningContainerName := range runningContainers {
-		if containerName == runningContainerName {
-			containerIsRunning = true
+func (c Container) IsPresent(containers []Container) bool {
+	for _, other := range containers {
+		if other.Name == c.Name {
+			return true
 		}
 	}
-	return containerIsRunning
+
+	return false
 }
 
-func containerName(key ConsulKey) string {
-	keyParts := strings.Split(key.Key, "/")
-	return keyParts[len(keyParts)-1]
-}
-
-func imageName(key ConsulKey) string {
-	base64Value := key.Value
-	decoded, _ := base64.StdEncoding.DecodeString(base64Value)
-	return string(decoded)
-}
-
-func runContainer(containerName string, imageName string) {
-	fmt.Printf("Running container with name '%s' with image '%s'\n", containerName, imageName)
-	_, err := exec.Command("docker", "run", "-d", "--name", containerName, imageName).Output()
+func (c Container) Run() {
+	fmt.Printf("Running container with name '%s' with image '%s'\n", c.Name, c.Image)
+	_, err := exec.Command("docker", "run", "-d", "-P", "--name", c.Name, c.Image).Output()
 
 	if err != nil {
 		fmt.Println("ERROR: Could not run docker run successfully")
 	}
 }
 
-func stopContainer(containerName string) {
-	fmt.Printf("Stopping container with name '%s'\n", containerName)
-	_, stopErr := exec.Command("docker", "stop", containerName).Output()
+func (c Container) Stop() {
+	fmt.Printf("Stopping container with name '%s'\n", c.Name)
+	_, stopErr := exec.Command("docker", "stop", c.Name).Output()
 
 	if stopErr != nil {
 		fmt.Println("ERROR: Could not run docker stop successfully")
@@ -159,56 +204,45 @@ func stopContainer(containerName string) {
 
 	time.Sleep(time.Second)
 
-	_, rmErr := exec.Command("docker", "rm", containerName).Output()
+	_, rmErr := exec.Command("docker", "rm", c.Name).Output()
 
 	if rmErr != nil {
 		fmt.Println("ERROR: Could not run docker rm successfully")
 	}
 }
 
-func handleStateChange(previousState KeyState, newState KeyState) {
+func handleChange(newState ConsulState) {
 	// TODO: This find all keys in namespace that differ.
 	// We want to only find 'app' keys
-	removedKeys := keyDiff(previousState.Keys, newState.Keys)
-	addedKeys := keyDiff(newState.Keys, previousState.Keys)
 
-	fmt.Println("Removed:")
-	fmt.Println(removedKeys)
-	fmt.Println("Added:")
-	fmt.Println(addedKeys)
-
-	if len(addedKeys) == 0 && len(removedKeys) == 0 {
-		return
-	}
-
-	runningContainers, err := runningContainers()
+	desired := newState.Containers()
+	current, err := runningContainers()
 
 	if err != nil {
 		//TODO should we handle this more gracefully
-		fmt.Println(err)
+		fmt.Printf("could not fetch running containers: %v\n", err)
 		return
 	}
 
-	if len(addedKeys) > 0 {
-		for _, key := range addedKeys {
-			containerName := containerName(key)
-			containerIsRunning := containerIsRunning(containerName, runningContainers)
+	removed := diffContainers(current, desired)
+	added := diffContainers(desired, current)
 
-			if !containerIsRunning {
-				imageName := imageName(key)
-				runContainer(containerName, imageName)
-			}
+	fmt.Printf("Removed: %v\n", removed)
+	fmt.Printf("Added: %v\n", added)
+
+	if len(added) == 0 && len(removed) == 0 {
+		return
+	}
+
+	if len(added) > 0 {
+		for _, container := range added {
+			container.Run()
 		}
 	}
 
-	if len(removedKeys) > 0 {
-		for _, key := range removedKeys {
-			containerName := containerName(key)
-			containerIsRunning := containerIsRunning(containerName, runningContainers)
-
-			if containerIsRunning {
-				stopContainer(containerName)
-			}
+	if len(removed) > 0 {
+		for _, container := range removed {
+			container.Stop()
 		}
 	}
 }
@@ -221,22 +255,22 @@ func main() {
 		panic("Must provide HOSTNAME and CONSUL_HOST env variables")
 	}
 
-	state := KeyState{Keys: []ConsulKey{}, Index: 0}
+	lastIndex := 0
 
 	for {
-		newState := KeyState{}
-		url := makeUrl(consulHost, hostname, state.Index)
-		err := getKeyState(&newState, url)
+		desiredState := ConsulState{}
+		url := makeUrl(consulHost, hostname, lastIndex)
+		err := getConsulStateBlocking(&desiredState, url)
 
 		if err != nil {
 			fmt.Println("There was an error getting the new state")
 			syscall.Exit(1)
 		}
 
-		handleStateChange(state, newState)
+		handleChange(desiredState)
 
 		time.Sleep(time.Second)
 
-		state = newState
+		lastIndex = desiredState.Index
 	}
 }
