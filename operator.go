5c00c57a718ad669443091c763942b3ba44808f9
Ryan Levick <ryan.levick@microsoft.com>
2015-12-09 11:39:24 +0100

    Bootstrap containers

--- operator.go
+++ operator.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"time"
 )
 
@@ -40,10 +43,69 @@ func loop(stateUrl *ConsulStateURL, servicesUrl *ConsulServicesURL) {
 	}
 }
 
+type BootImage struct {
+	Name  string `json:"name"`
+	Image string `json:"image"`
+}
+
+func (b BootImage) ToContainer() Container {
+	return Container{Name: b.Name, Image: b.Image}
+}
+
+type Config struct {
+	BootImages []BootImage `json:"boot_images"`
+}
+
+func config(configPath string) (*Config, error) {
+	contents, err := ioutil.ReadFile(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	jsonErr := json.NewDecoder(bytes.NewReader(contents)).Decode(config)
+
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return config, nil
+}
+
+func bootstrapContainers(config *Config) []Container {
+	var containers []Container
+	for _, bootImage := range config.BootImages {
+		bootImage.ToContainer()
+	}
+
+	return containers
+}
+
+func runBootstrapContainers(containers []Container) {
+	for _, container := range containers {
+		container.Run()
+	}
+}
+
+func bootstrap(configPath string) ([]Container, error) {
+	config, err := config(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	containers := bootstrapContainers(config)
+	runBootstrapContainers(containers)
+
+	return containers, nil
+}
+
 func main() {
 	var (
 		hostname   = flag.String("hostname", "", "The name of the host which is running operator")
 		consulHost = flag.String("consulhost", "", "The name of the consul host")
+		configPath = flag.String("configpath", "./operator.json", "The path to the operator.json. The default is the current directory of the binary.")
 		shouldLoop = flag.Bool("loop", false, "Run on each change to the consul key/value storage")
 		wait       = flag.String("wait", "5m", "The timeout for polling")
 	)
@@ -51,6 +113,12 @@ func main() {
 
 	if *hostname == "" && *consulHost == "" {
 		panic("ERROR: Must provide hostname and consulhost flags")
+	}
+
+	_, err := bootstrap(*configPath)
+
+	if err != nil {
+		panic("ERROR: Could not bootstrap containers")
 	}
 
 	stateUrl := ConsulStateURL{Host: *consulHost, Hostname: *hostname, Wait: *wait}
